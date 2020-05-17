98c6c56bbc74b1b4ec544b8c101c4ec6e4677414
Tanuj Joshi <tanuj.inn@gmail.com>
2020-05-17 15:15:12 +0530

    Console App v1

---
+++ ConsoleBasedApp/src/soperaAssignementConsoleApp/SoperaConsoleApp.java
@@ -0,0 +1,54 @@
+package soperaAssignementConsoleApp;
+
+import java.util.Scanner;
+
+public class SoperaConsoleApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		showMainMenu();
+	}
+	
+	public static void showMainMenu() {
+		final Scanner option = new Scanner(System.in);
+		int count = 1;
+		int deletedTaskCount = 1;
+		SoperaConsoleAppFunc singletaskObj = new SoperaConsoleAppFunc();
+		while (true) {
+			System.out.println("\n++++++++++++ TO DO LIST APP ++++++++++++\n");
+			System.out.println("1. Enter a single Task");
+			System.out.println("2. Enter Multiple Tasks");
+			System.out.println("3. Show All the Tasks");
+			System.out.println("4. Delete the Task");
+			System.out.println("5. Track Delete Task");
+			System.out.println("\n++++++++++++ Please Enter the Choice ++++++++++++\n");
+			switch(option.nextInt()) {
+				case 1:
+					System.out.println("\n*************** Entering Single Task ***************\n");
+					singletaskObj.singleTask(count++);
+					break;
+				case 2:
+					System.out.println("\n*************** Entering Multiple Task ***************\n");
+					count = singletaskObj.multipleTask(count);
+					break;
+				case 3:
+					System.out.println("\n*************** Show Task ***************\n");
+					//singletaskObj = new SoperaConsoleAppFunc();
+					singletaskObj.listTask();					
+					break;
+				case 4:
+					System.out.println("\n*************** Delete Task ***************\n");
+					singletaskObj.deleteTask();
+					break;
+				case 5:
+					System.out.println("\n*************** Track Deleted Task ***************\n");
+					singletaskObj.trackDeletedTask();
+					break;	
+				default:
+					System.out.println("\n*************** Wrong Choice ***************\n");
+					break;					
+			}
+		}
+	}
+
+}
---
+++ ConsoleBasedApp/src/soperaAssignementConsoleApp/SoperaConsoleAppFunc.java
@@ -0,0 +1,62 @@
+package soperaAssignementConsoleApp;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.Set;
+import java.util.TreeMap;
+
+public class SoperaConsoleAppFunc {
+		
+	TreeMap<Integer, String> taskMap = new TreeMap<Integer, String>();
+	TreeMap<Integer, String> deletedTaskMap = new TreeMap<Integer, String>();
+	Scanner taskString = new Scanner(System.in);
+	
+	//Method to handle Single task
+	public void singleTask(int count) {
+		String tasks = taskString.nextLine();
+		taskMap.put(count, tasks);		
+	}
+
+	//Method to handle Multiple task
+	public int multipleTask(int count) {
+        Scanner input = new Scanner(System.in);
+        String tasks;
+        List<String> multipleTask = new ArrayList<String>();
+        while (!(tasks = input.nextLine()).isEmpty()) {
+        	multipleTask.add(tasks);
+        }
+        for(String multTask : multipleTask) {
+        	taskMap.put(count, multTask);
+        	count++;
+        }
+        return count;
+    }
+	
+	//Method to List Task
+	public void listTask() {
+		Set<Map.Entry<Integer, String>> setTm = taskMap.entrySet();
+		for(Map.Entry<Integer, String> me : setTm) {
+			System.out.println("------------> " + me.getKey() + " : "+me.getValue());
+			//System.out.println(me.getValue());
+		}
+	}
+	
+	//Method to Delete Task
+	public void deleteTask() {
+		Scanner input = new Scanner(System.in);
+		System.out.println("Please Enter Task Number to Delete the Task");
+		int key = input.nextInt();
+		String taskName = (String)taskMap.remove(key); 
+		deletedTaskMap.put(key, taskName);
+	}
+	
+	//Method to handle Track Deleted Task
+	public void trackDeletedTask() {
+		Set<Map.Entry<Integer, String>> setTm = deletedTaskMap.entrySet();
+		for(Map.Entry<Integer, String> me : setTm) {
+			System.out.println("------------> " + me.getKey() + " : "+me.getValue());
+		}		
+	}	
+}
