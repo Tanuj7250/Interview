98b8c94d7f512df82b472efeb3cd0a07ef1a8a0c
Tanuj Joshi <tanuj.inn@gmail.com>
2020-05-31 11:27:06 +0530

    To check if Key Exist while deleting task

--- ConsoleBasedApp/src/sopraAssignementConsoleApp/SopraConsoleAppFunc.java
+++ ConsoleBasedApp/src/sopraAssignementConsoleApp/SopraConsoleAppFunc.java
@@ -13,43 +13,43 @@ import java.util.Set;
 import java.util.TreeMap;
 
 public class SopraConsoleAppFunc {
-		
+
 	TreeMap<Integer, String> taskMap = new TreeMap<Integer, String>();
 	TreeMap<Integer, String> deletedTaskMap = new TreeMap<Integer, String>();
 	Scanner taskString = new Scanner(System.in);
 	final static String path = "D:\\Softwares\\TaskList-data.txt";
-	
+
 	public int  getHashMapFromTextFile(){
 		BufferedReader br = null;
 		try{
-            File file = new File(path);
-            br = new BufferedReader( new FileReader(file) );
-            String line = null;
-            while ( (line = br.readLine()) != null ){
-            	String[] parts = line.split(":");
-            	Integer key = Integer.parseInt(parts[0].trim());
-            	String task = parts[1].trim();
-            	if( !key.equals("") && !task.equals("") ) {
-            		taskMap.put(key, task);
-            		saveTask();
-            	}
-            }
+			File file = new File(path);
+			br = new BufferedReader( new FileReader(file) );
+			String line = null;
+			while ( (line = br.readLine()) != null ){
+				String[] parts = line.split(":");
+				Integer key = Integer.parseInt(parts[0].trim());
+				String task = parts[1].trim();
+				if( !key.equals("") && !task.equals("") ) {
+					taskMap.put(key, task);
+					saveTask();
+				}
+			}
 		}catch(Exception e){
-            e.printStackTrace();
-        }finally{
-            if(br != null){
-                try { 
-                    br.close(); 
-                }catch(Exception e){};
-            }
-        }
-        if(!taskMap.isEmpty()) {
-        	return taskMap.lastKey() + 1;
-        }else {
-        	return 1;
-        }
+			e.printStackTrace();
+		}finally{
+			if(br != null){
+				try { 
+					br.close(); 
+				}catch(Exception e){};
+			}
+		}
+		if(!taskMap.isEmpty()) {
+			return taskMap.lastKey() + 1;
+		}else {
+			return 1;
+		}
 	}
-	
+
 	//Method to handle Single task
 	public void singleTask(int count){
 		String tasks = taskString.nextLine();
@@ -59,54 +59,59 @@ public class SopraConsoleAppFunc {
 
 	//Method to handle Multiple task
 	public int multipleTask(int count){
-        Scanner input = new Scanner(System.in);
-        String tasks;
-        List<String> multipleTask = new ArrayList<String>();
-        while (!(tasks = input.nextLine()).isEmpty()) {
-        	multipleTask.add(tasks);
-        }
-        for(String multTask : multipleTask) {
-        	taskMap.put(count, "[] " +multTask);
-        	count++;
-        }
-        saveTask();
-        return count;
-    }
-	
+		Scanner input = new Scanner(System.in);
+		String tasks;
+		List<String> multipleTask = new ArrayList<String>();
+		while (!(tasks = input.nextLine()).isEmpty()) {
+			multipleTask.add(tasks);
+		}
+		for(String multTask : multipleTask) {
+			taskMap.put(count, "[] " +multTask);
+			count++;
+		}
+		saveTask();
+		return count;
+	}
+
 	//Method to List Task
 	public void listTask(){
-			//BufferedWriter bw = null;
-			Set<Map.Entry<Integer, String>> setTm = taskMap.entrySet();
-			for(Map.Entry<Integer, String> me : setTm) {
-				System.out.println("------------> " + me.getKey() + " : "+me.getValue());
+		//BufferedWriter bw = null;
+		Set<Map.Entry<Integer, String>> setTm = taskMap.entrySet();
+		for(Map.Entry<Integer, String> me : setTm) {
+			System.out.println("------------> " + me.getKey() + " : "+me.getValue());
 
-			}	
+		}	
 	}
-	
+
 	//Method to Delete Task
 	public void deleteTask(){
 		Scanner input = new Scanner(System.in);
 		System.out.println("Please Enter Task Number to Delete the Task");
 		int key = input.nextInt();
-		String taskName = (String)taskMap.remove(key); 
-		ArrayList<Integer> newKey = new ArrayList<Integer>();
-		ArrayList<String> newValue = new ArrayList<String>();
-		deletedTaskMap.put(key, taskName);
-		Set<Map.Entry<Integer, String>> setTm = taskMap.tailMap(key).entrySet();
-		for(Map.Entry<Integer, String> me : setTm) {
-			newKey.add(me.getKey()-1);
-			newValue.add(me.getValue());
+
+		if(taskMap.containsKey(key)) {
+			String taskName = (String)taskMap.remove(key); 
+			ArrayList<Integer> newKey = new ArrayList<Integer>();
+			ArrayList<String> newValue = new ArrayList<String>();
+			deletedTaskMap.put(key, taskName);
+			Set<Map.Entry<Integer, String>> setTm = taskMap.tailMap(key).entrySet();
+			for(Map.Entry<Integer, String> me : setTm) {
+				newKey.add(me.getKey()-1);
+				newValue.add(me.getValue());
+			}
+			for(int i = 0; i < newKey.size(); i++) {
+				taskMap.put(newKey.get(i), newValue.get(i));
+
+			}
+			taskMap.remove(taskMap.lastKey());
+			saveTask();
+		}else {
+			System.out.println("KEY DOES NOT EXIST ");
 		}
-		for(int i = 0; i < newKey.size(); i++) {
-			taskMap.put(newKey.get(i), newValue.get(i));
-			
-		}
-		taskMap.remove(taskMap.lastKey());
-		saveTask();
 		//System.out.println("newKey "+newKey);
 		//System.out.println("newValue "+newValue);
 	}
-	
+
 	//Method to handle Track Deleted Task
 	public void trackDeletedTask(){
 		String deletedPath = "D:\\Softwares\\Deleted-TaskList-data.txt";
@@ -122,7 +127,7 @@ public class SopraConsoleAppFunc {
 			e.printStackTrace();
 		}
 	}	
-	
+
 	public void markTaskCompleted(){
 		Scanner input = new Scanner(System.in);
 		System.out.println("Please Enter Task Number which is Completed");
@@ -134,7 +139,7 @@ public class SopraConsoleAppFunc {
 			System.out.println("TASK Do not Exist");
 		}
 	}
-	
+
 	public void saveTask() {
 		String outputFilePath = path;
 		try {
@@ -148,5 +153,5 @@ public class SopraConsoleAppFunc {
 			e.printStackTrace();
 		}
 	}
-		
+
 }
